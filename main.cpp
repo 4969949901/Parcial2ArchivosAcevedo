c87a9246c2397af6b042fed23c09adead1bd3c5d
Helen <lissa.acevedo99@gmail.com>
2023-05-06 09:42:47 -0600

    parcial2archivosAcevedolibrerias

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <filesystem>
+#include <windows.h>
+#include <string>
+
+void crearArchivos();
+void agregarTexto();
+void buscarArchivo();
+
+using namespace std;
+
+int main() {
+    crearArchivos();
+    agregarTexto();
+    buscarArchivo();
+
+    system("pause");
+    return 0;
+}
+
+void crearArchivos(){
+
+
+
+}
+
+void agregarTexto(){
+
+}
+
+void buscarArchivo(){
+
+}
